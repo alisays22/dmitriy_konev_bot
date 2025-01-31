3c1e81ae2eec526439ced884d40b49fec31a0a7c
alisays22 <109974671+alisays22@users.noreply.github.com>
2025-01-31 20:17:16 +0400

    Delete composer/hears/one.hears.js

--- composer/hears/one.hears.js
+++
@@ -1,9 +0,0 @@
-const { Telegraf} = require("telegraf");
-require("dotenv").config();
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-
-bot.action("addService", async(ctx) => ctx.scene.enter("oneWizard"));
-
-// Export the bot instance
-module.exports = bot;
